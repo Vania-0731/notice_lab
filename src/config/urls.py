aa65c2192af69e240e9dbeb51619b998b291434a
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-04-23 14:58:00 -0500

    Urls for profiles

--- src/config/urls.py
+++ src/config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
+    path("accounts/", include("profiles.urls")),
 ]
 
 if settings.DEBUG:
